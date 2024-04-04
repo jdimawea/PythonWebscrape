e9183f53a5bb5ba56503a4fb63946823b032386d
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-04 01:19:29 -0600

    BeautifulSoup

--- BeautifulSoup/bSb.py
+++ BeautifulSoup/bSb.py
@@ -1,4 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 
+# assigning a variable to the url
+website = 'https://subslikescript.com/movie/Titanic-120338'
+# Fetching the page
+result = requests.get(website)
+# This is the page content
+content = result.text
+# creating the soup
+soup = BeautifulSoup(content, 'lxml')
+# Displays a nice version of the html of the page
+print(soup.prettify())
 
+soup.find('article', class_="main-article")
